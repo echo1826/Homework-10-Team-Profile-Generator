7c66bd63ffb1d8ed425bf9134cec32482cd038f4
Ethan C <ethan.cho1826@gmail.com>
2021-10-15 23:35:22 -0500

    validation function and getEmail method

--- index.js
+++ index.js
@@ -6,55 +6,80 @@ const Intern = require('./lib/intern');
 const renderHtml = require('./lib/htmlrender');
 const renderCss = require('./lib/cssrender');
 
-
 const teamMembers = [];
+
+const isValid = (value) => {
+    if (value === "") {
+        return "Please type in an answer";
+    } else {
+        return true;
+    }
+}
 
 const questions = [{
         type: "list",
         message: "Choose the role of the employee:",
         choices: ["Manager", "Intern", "Engineer"],
-        name: "employee"
+        name: "employee",
     },
     {
         type: "input",
         message: "What is the name of the employee?",
-        name: "employeeName"
+        name: "employeeName",
+        validate: isValid
     },
     {
         type: "input",
         message: "What is the ID for the employee?",
-        name: "id"
+        name: "id",
+        validate: isValid
     },
     {
         type: "input",
         message: "What is the email for the employee?",
-        name: "email"
+        name: "email",
+        validate(value) {
+            const pass = value.includes("@");
+            if(pass) {
+                return true;
+            }else {
+                return "Please enter a valid email";
+            }
+        }
     },
     {
         type: "input",
         message: "What is the office number for the manager?",
         name: "officeNumber",
+        validate: isValid,
         when: (answer) => answer.employee === "Manager"
     },
     {
         type: "input",
         message: "What is the github for the engineer?",
         name: "github",
+        validate: isValid,
         when: (answer) => answer.employee === "Engineer"
     },
     {
         type: "input",
         message: "What is the school of the intern?",
         name: "school",
+        validate: isValid,
         when: (answer) => answer.employee === "Intern"
     },
     {
         type: "confirm",
         message: "Add a member or quit?",
         // choices:["Add member", "Quit"],
+        validate: isValid,
         name: "isFinished"
     }
 ];
+
+// const managerQuestions = [
+//     we;kljso
+// ]
 
 // functions that generate new objects based on user answers
 function generateIntern(data) {
@@ -104,6 +129,15 @@ function decideEmployee(data) {
     }
 }
 
+// inquire
+//     .prompt(managerQuestions)
+//     .then(response => {
+//         //function calls
+//         inquire.prompt(questions).then(data => {
+//             //function calls
+//         })
+//     })
+
 // asks user questions then calls function that decides what happens based on the data received
 function employeeAnswers() {
     inquire
@@ -121,7 +155,7 @@ function employeeAnswers() {
                 employeeAnswers();
             }
             // writeCss(renderCss());
-            
+
         }));
 }
 
